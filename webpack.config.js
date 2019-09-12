ae6ab365906c6119d1095ad2fe440f98e3fb16e9
erib <eri-b@users.noreply.github.com>
2019-09-12 13:24:55 -0400

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   mode: 'development',
@@ -7,5 +7,5 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-  watch: true,
-};
+  watch: true
+}
